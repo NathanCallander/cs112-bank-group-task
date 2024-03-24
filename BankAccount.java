8f74c2900b0f77c4db5a4109e820dc08961edf66
CharleyMcKayStrath <158153669+CharleyMcKayStrath@users.noreply.github.com>
2024-03-24 17:34:10 +0000

    Created lowRate and highRate variables and added getters/setters

--- BankAccount.java
+++ BankAccount.java
@@ -1,2 +1,41 @@
-public class BankAccount {
+public abstract class BankAccount {
+
+    // declare lowRate & highRate
+    public static float lowRate;
+    public static float highRate;
+
+
+    // public getter/setter for highRate/lowRate
+    public static float getLowRate() {
+        return lowRate;
+    }
+
+    public static void setLowRate(float rate) {
+        if (rate < 0) {
+            lowRate = 0;
+        } else if (rate > highRate) {
+            lowRate = highRate;
+        } else {
+            lowRate = rate;
+        }
+    }
+
+    public static float getHighRate() {
+        return highRate;
+    }
+
+    public static void setHighRate(float rate) {
+        if (rate > 10) {
+            highRate = 10;
+        } else if (rate < lowRate) {
+            highRate = lowRate;
+        } else {
+            highRate = rate;
+        }
+    }
+
+
+
+
+
 }
