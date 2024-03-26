411c62299529c1565af4088627898eed17909366
Angelina <angelina.p20056@gmail.com>
2024-03-26 20:07:13 +0000

    SavingsAccountTest added

--- src/SavingsAccountTest.java
+++ src/SavingsAccountTest.java
@@ -3,14 +3,11 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SavingsAccountTest {
-
-    private SavingsAccount savingsAccount;
-
     @Test
     public void setRateTest(){
         BankAccount.setHighRate(9f);
         BankAccount.setLowRate(1f);
-        savingsAccount = new SavingsAccount("test","12345",5f);
+        SavingsAccount savingsAccount = new SavingsAccount("test","12345",5f);
 
         assertEquals(5f,savingsAccount.getRate(), 0.0);
     }
@@ -19,7 +16,7 @@ public class SavingsAccountTest {
     public void setRateLowTest(){
         BankAccount.setHighRate(9f);
         BankAccount.setLowRate(1f);
-        savingsAccount = new SavingsAccount("test","12345",11f);
+        SavingsAccount savingsAccount = new SavingsAccount("test","12345",11f);
 
         assertEquals(9f,savingsAccount.getRate(), 0.0);
     }
@@ -28,7 +25,7 @@ public class SavingsAccountTest {
     public void setRateHighTest(){
         BankAccount.setHighRate(9f);
         BankAccount.setLowRate(1f);
-        savingsAccount = new SavingsAccount("test","12345",-1f);
+        SavingsAccount savingsAccount = new SavingsAccount("test","12345",-1f);
 
         assertEquals(1f,savingsAccount.getRate(), 0.0);
     }
@@ -37,7 +34,7 @@ public class SavingsAccountTest {
     public void setRateEdgeTest(){
         BankAccount.setHighRate(9f);
         BankAccount.setLowRate(1f);
-        savingsAccount = new SavingsAccount("test","12345",9f);
+        SavingsAccount savingsAccount = new SavingsAccount("test","12345",9f);
 
         assertEquals(9f,savingsAccount.getRate(), 0.0);
     }
@@ -46,8 +43,29 @@ public class SavingsAccountTest {
     public void addInterestTest(){
         BankAccount.setHighRate(9f);
         BankAccount.setLowRate(1f);
-        savingsAccount = new SavingsAccount("test","12345",6f);
+        SavingsAccount savingsAccount = new SavingsAccount("test","12345",6f);
+        savingsAccount.setBalance(1000f);
         savingsAccount.addInterest();
-        assertEquals(10f,savingsAccount.addInterest(),0.0);
+        assertEquals(1060.0f,savingsAccount.getBalance(), 0.0);
+    }
+
+    @Test
+    public void addInterestExtremeHighTest(){
+        BankAccount.setHighRate(9f);
+        BankAccount.setLowRate(1f);
+        SavingsAccount savingsAccount = new SavingsAccount("test","12345",11f);
+        savingsAccount.setBalance(1000f);
+        savingsAccount.addInterest();
+        assertEquals(1090.0,savingsAccount.getBalance(), 0.0);
+    }
+
+    @Test
+    public void addInterestExtremeLowTest(){
+        BankAccount.setHighRate(9f);
+        BankAccount.setLowRate(1f);
+        SavingsAccount savingsAccount = new SavingsAccount("test","12345",-1f);
+        savingsAccount.setBalance(1000f);
+        savingsAccount.addInterest();
+        assertEquals(1010.0,savingsAccount.getBalance(), 0.0);
     }
 }
